98bc506086962928e1cb0d393ee612f52b148111
maxibj-404 <YOUR_GITHUB_EMAIL>
2026-03-19 17:29:35 +0100

    Change practices to 3 columns layout

--- src/sections/BlogPosts.tsx
+++ src/sections/BlogPosts.tsx
@@ -86,7 +86,7 @@ export function BlogPosts() {
             </div>
 
             {/* Blog Cards Grid */}
-            <div className="grid md:grid-cols-2 gap-6">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
               {filteredPieces.map(piece => (
                 <article 
                   key={piece.id} 
